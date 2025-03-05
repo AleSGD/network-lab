b3ce8391bc81666253676f8effc4fb1dc7f8ae49
soliz.gab.ale@gmail.com <soliz.gab.ale@gmail.com>
2025-03-04 22:59:24 -0300

    Solucionamos problemas con el puesto :53. Modificamos el script de inicio dnsmasq para que detenga systemd-resolved

--- Escenario1-PortForwarding/start_dnsmasq.sh
+++ Escenario1-PortForwarding/start_dnsmasq.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+#Vamos a chequear el estado de systemd-resolved ya que utiliza el puerto :53 y nos genera inconvenientes
+#Chequeo systemd-resolved
+if  systemctl is-active systemd-resolved ; then
+	echo "systemd-resolved esta activo."
+
+	#Si esta activo, desabilitarlo.
+	echo "Deshabilitando systemd-resolved..."
+	sudo systemctl disable systemd-resolved
+	echo "systemd-resolved deshabilitado."
+
+	#Detener systemd-resolved.
+	echo "Deteniendo systemd-resolved...."
+	sudo systemctl stop systemd-resolved
+	echo "systemd-resolved se detuvo."
+else
+	echo "systemd-resolved esta detenido y deshabilitado."
+fi
+
 #Inicia el servidor dnsmasq
 sudo systemctl start dnsmasq
 if [ $? -ne 0 ]; then
@@ -13,4 +31,6 @@ if [ $? -ne 0 ]; then
 	echo "Error al iniciar el servidor HTTP."
 fi
 
-echo "Servidores iniciados"
+echo "Servidor DNS dnsmasq iniciado"
+
+exit 0
